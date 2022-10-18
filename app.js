b2bb9c10e4dd63cf8f8d5a6805ac3d63f04a6dea
normstraker <88446816+normstraker@users.noreply.github.com>
2022-10-18 15:54:25 -0500

    Variables

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-// document.write('hello world');
-// alert('hello world');
-// console.log('hello world')
+// Variable - Most basic building block
+// Variables - Store, access, modify === value
+// Declare, assignment operator, assign value
+// assign value later, modify existing
+// can contain digits, letters, underscore, $
+// must start with a letter, $ or _
+
+// cannot use keywords as variables
+// cannot start with a number
+
+// case sensitive - fullname vs Fullname
+
+// camelCase or underscore (full_name)
+
+// using const (CONSTANT) - can't be reassigned
+// cannot assign a const without a value as you can with let
+
+let name = "norm";
+let address, zip, state, city;
+address = "101 main street"
+zip = "76777"
+city = "Saginaw"
+state = "MI"
+console.log(name, address, city, state, zip)
