ced7ace83cf419d0aeae815a3c4c2b0cc89d92d4
normstraker <88446816+normstraker@users.noreply.github.com>
2022-10-19 17:26:37 -0500

    Arrays challenge

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
-// Arrays, Functions and Objects
-// Arrays = [], 0 index based
+// Arrays Challenge #4
 
-const friend1 = "anna";
-const friend2 = "bob";
-const friend3 = "carla";
-const friend4 = "david";
-
-const friends = ["john", "peter", "bob", "susie", 45, undefined, null];
-
-let bestFriend = friends[3];
-friends[4] = "anna";
-console.log(friends[4]);
-console.log(friends);
-console.log(friends[0]);
-console.log(bestFriend);
+const fruits = ['apple', 'banana', 'coconut', 'date', 45]
+const firstFruit = fruits[0]
+fruits[4] = 'fig'
+console.log(firstFruit, fruits)
