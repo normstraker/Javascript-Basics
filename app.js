03ef19b687f6312ed112e182b41fcff48139204a
normstraker <88446816+normstraker@users.noreply.github.com>
2022-10-19 18:28:53 -0500

    Functions - Return

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 // Arrays, Functions and Objects
-// params - when declare/define
-// placeholders, local vars
-// arguments - when invoke/call/run
-// use var/values, multiple params, undefined
-// local variables - only used within the function
+// return
+// default undefined, shorcuts, ignores after
+// 1 inch = 2.54cm (inch * 2.54)
 
-function greet(name) {
-  console.log(`Hello there ${name}!`);
+const wallHeight = 80;
+const wallWidth = 100;
+
+function calculate(value) {
+  //   const newValue = value * 2.54;
+  //  return newValue;
+  //               or
+  return value * 2.54;
 }
 
-greet("Jonathan");
-greet("Mattia");
-greet("Perambulathu");
+// returns the result of the function to the const
+const width = calculate(wallWidth);
+const height = calculate(wallHeight);
+
+// array is populated with the const's results
+const dimensions = [width, height];
+console.log(
+  dimensions[0] + " is the width and " + dimensions[1] + " is the height."
+);
+console.log(
+  `${dimensions[0]} is the width and ${dimensions[1]} is the height.`
+);
