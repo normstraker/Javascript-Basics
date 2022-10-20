49b0f533aed7cb38b23fc1e092e9d7954e8f36ca
normstraker <88446816+normstraker@users.noreply.github.com>
2022-10-20 17:35:41 -0500

    Object Challenge

--- app.js
+++ app.js
@@ -18,9 +18,29 @@ const person = {
 };
 
 const age = person.age;
-console.log(age)
-person.name = "bob"
+console.log(age);
+person.name = "bob";
 console.log(person.name);
 console.log(person.siblings[2]);
 // run the method
 person.greeting();
+
+// Object Challenge
+
+const car = {
+  make: "Honda",
+  model: "Accord",
+  year: 2017,
+  colors: ["white", "tan", "black"],
+  hybrid: false,
+  drive() {
+    console.log("smooth drive with lots of power");
+  },
+  stop() {
+    console.log("advanced braking");
+  },
+};
+console.log(car.make);
+console.log(car.colors[0]);
+car.drive();
+car.stop();
