4bcc28370ae2b2da0f3e1293151713baf89d7c63
normstraker <88446816+normstraker@users.noreply.github.com>
2022-10-20 12:07:53 -0500

    Function Expressions

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
 // Arrays, Functions and Objects
-// return
-// default undefined, shorcuts, ignores after
-// 1 inch = 2.54cm (inch * 2.54)
+// expressions - another way to define a function
+// create a variable, assign to a FUNCTION (not value), use var
+// diff - hoisting, use - arrow func, libraries
 
-const wallHeight = 80;
-const wallWidth = 100;
-
-function calculate(value) {
-  //   const newValue = value * 2.54;
-  //  return newValue;
-  //               or
-  return value * 2.54;
+// function definition/declaration
+function addValues(num1, num2) {
+  return num1 + num2;
 }
 
-// returns the result of the function to the const
-const width = calculate(wallWidth);
-const height = calculate(wallHeight);
+const firstValue = addValues(3, 4);
+const secondValue = addValues(12, 34);
 
-// array is populated with the const's results
-const dimensions = [width, height];
-console.log(
-  dimensions[0] + " is the width and " + dimensions[1] + " is the height."
-);
-console.log(
-  `${dimensions[0]} is the width and ${dimensions[1]} is the height.`
-);
+// function expression (you can omit (or keep) the name "addValues" since the value is now in the const "add") - anonomous function
+const add = function (num1, num2) {
+  return num1 + num2;
+};
+
+// const thirdValue = add(5, 6);
+const values = [firstValue, secondValue, add(5, 6)];
+console.log(values);
+
+// arrow function (only as an expression)
+const multiply = (num1, num2) => num1 * num2
