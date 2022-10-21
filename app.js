f75188150c5391f7c3c98f261f5bd853eaa19f38
normstraker <88446816+normstraker@users.noreply.github.com>
2022-10-21 16:34:30 -0500

    Equality

--- app.js
+++ app.js
@@ -2,34 +2,16 @@
 // Comparison Operators
 // >, <, >=, <=, ==, ===, !=, !===
 // else if and !
+// = is for assignment only
+// == checks only value
+// === check value and type
 
-// =============================
-// const value = 2 > 1;
-// console.log(typeof value)
-// const value2 = false;
+const num1 = 6
+const num2 = '6';
 
-// if(value){ // if value is true
-//   console.log('hello wolrd')
-// } else { // if value is false
-//   console.log('hello peolpe')
-// }
-// =============================
+const value = num1 != num2;
+const value2 = num1 !== num2;
 
-const num1 = 6;
-const num2 = 6;
-const result = num1 >= num2;
-
-const value = false
-
-if(!value){
-  console.log('value is false')
-}
-
-// if (num1 > num2) {
-//   console.log("first number is larger than second number");
-// } else if (result) {
-//   // num1 >= num2
-//   console.log("first number is equal to second number");
-// } else {
-//   console.log("second number is larger than first number");
-// }
+console.log(value);
+console.log(value2);
+ 
