0854b6e1f9a7caad383335a4f911bbb9ae02ecd1
normstraker <88446816+normstraker@users.noreply.github.com>
2022-10-21 17:19:46 -0500

    While Loops

--- app.js
+++ app.js
@@ -1,25 +1,11 @@
-// Conditional Statements Challenge
+// Loops
+// repeatedly run a block of code while the condition is met or true
+// while loop
+// TURN OFF AUTOSAVE
 
-const person1 = {
-  name: "nrom",
-  age: 17,
-  status: "tourist",
-};
+let amount = 10;
 
-const person2 = {
-  name: "shrilye",
-  age: 55,
-  status: "resident",
-};
-
-if (person1.age >= 18 && person1.status === "resident") {
-  console.log("welcome to the usa");
-} else {
-  console.log("sorry, no entry");
-}
-
-if (person2.age >= 18 && person2.status === "resident") {
-  console.log("welcome to the usa");
-} else {
-  console.log("sorry, no entry");
+while (amount > 0) {
+  console.log(`I have ${amount} dollars and I'm going to the mall`);
+  amount--;
 }
