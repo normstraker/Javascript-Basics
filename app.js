7135d8c245f93a6d5c04779438d05fe29e8d8faa
normstraker <88446816+normstraker@users.noreply.github.com>
2022-10-21 16:40:42 -0500

    Logical Operators

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
-// Conditional Statements
-// Comparison Operators
-// >, <, >=, <=, ==, ===, !=, !===
-// else if and !
-// = is for assignment only
-// == checks only value
-// === check value and type
+// Logical Operators
+// (|| - OR), (&& - AND), !
 
-const num1 = 6
-const num2 = '6';
+const name = "bob";
+const age = 23;
 
-const value = num1 != num2;
-const value2 = num1 !== num2;
-
-console.log(value);
-console.log(value2);
- 
+if(name === "bob" && age !== 24){
+  console.log('hello there user')
+} else {
+  console.log('wrong values')
+}
