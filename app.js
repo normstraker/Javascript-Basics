111025fdb5577f8ff8b771c6c027f9ed1c1d2d9b
normstraker <88446816+normstraker@users.noreply.github.com>
2022-10-24 18:06:11 -0500

    Strings Challenge

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-// Template Literals - ES6+
-// Backtick characters `` - above tab key
-// Interpolation ${} - insert expression(value)
+// Strings Challenge #8
 
-const name = 'john'
-const age = 25
-const sentence = "Hey it's " + name + ' and he is ' + age + " years old."
-const newSentence = `Hey it's ${name} and he is ${age} years old and ${4 + 4} is my brother's age.`
+// function fullName(firstName, lastName) {
+//   const fullName = `${firstName} ${lastName}`;
+//   return fullName.toUpperCase();
+// }
 
-console.log(sentence)
-console.log(newSentence)
+// console.log(fullName("Alexander", "Perambulathu"));
+
+function fullName({ firstName, lastName }) {
+  const fullName = `${firstName} ${lastName}`;
+  return fullName.toUpperCase();
+}
+
+// order doesn't matter
+console.log(fullName({ lastName: "Perambulathu", firstName: "Alexander" }));
