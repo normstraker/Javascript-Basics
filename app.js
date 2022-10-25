dcf93b6c24666d6ea9d421df370e0b92ee7f8b4a
normstraker <88446816+normstraker@users.noreply.github.com>
2022-10-25 17:25:58 -0500

    Exercise - Calculate Total

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-// Array and for loop
+// Functions, return, if, arrays, for loop
 
-const names = ["Walter", "Shirley", "Rick", "Vicki", "Jackie", "Pam", "Norm", "Jon"];
-const lastName = "Straker";
-let newArray = [];
+const gas = [20, 40, 100, 30];
+const food = [10, 40, 50];
 
-// for loop
-for (let i = 0; i < names.length; i++) {
-  console.log(i);
-  console.log(names[i]);
-  // const fullName = `${names[i]} ${lastName}`;
-  newArray.push(`${names[i]} ${lastName}`);
+function calculateTotal(arr) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  if(total > 100){
+    console.log(`Whoa! You are spending way too much!`)
+    return total;
+} 
+console.log(`You are good, total is less than $100.`)
+  return total;
 }
 
-console.log(names);
-console.log(newArray);
+const gasTotal = calculateTotal(gas);
+const foodTotal = calculateTotal(food);
+const randomTotal = calculateTotal([200, 4000, 500, 1]);
+
+console.log({
+  gas: gasTotal,
+  food: foodTotal,
+  random: randomTotal,
+});
